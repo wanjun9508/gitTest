5c9c4031dc98956f0017a29f02d14f5cd66b3f40
wanjun <wanjun@4px.com>
2020-09-01 10:29:03 +0800

    本地dev01提交

--- src/main/java/com/springboot/testgit/HelloController.java
+++ src/main/java/com/springboot/testgit/HelloController.java
@@ -10,6 +10,8 @@ public class HelloController {
     @RequestMapping("/hello")
     public String index() {
         System.out.println("dev01代码提交");
+        System.out.println("本地dev01代码提交dev");
+        System.out.println("本地dev012代码提交dev2");
         System.out.println("本地m分支提交到dev");
         return "Hello World";
     }
