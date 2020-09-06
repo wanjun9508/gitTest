ea2e170ede83c2a0628321601ad9edcd6be55a06
wanjun <1136339437@qq.com>
2020-09-06 21:07:51 +0800

    dev01代码提交

--- src/main/java/com/springboot/testgit/HelloController.java
+++ src/main/java/com/springboot/testgit/HelloController.java
@@ -10,10 +10,6 @@ public class HelloController {
     @RequestMapping("/hello")
     public String index() {
         System.out.println("dev01代码提交");
-        System.out.println("本地dev01代码提交dev");
-        System.out.println("本地dev012代码提交dev2");
-        System.out.println("本地m分支提交到dev");
-        System.out.println("测试撤销合并");
         return "Hello World";
     }
 
